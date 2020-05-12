eea511d4fc54613c006c852a1e4210f56529fc9c
Andrew Griffen <andrew.griffen@gmail.com>
2020-05-12 13:39:06 +0900

    Added new function print_all()

---
+++ R/print_all.R
@@ -0,0 +1,20 @@
+#' @importFrom magrittr %>%
+NULL
+
+#' A modified print function
+#'
+#' This function print all rows of a data frame
+#' @param
+#' @keywords print rows
+#' @export
+#' @examples
+#' print_all(mtcars)
+
+print_all <- function(df){
+  if(inherits(df, "tbl_df")){
+    df %>% print(n=nrow(df))
+  }else{
+    df %>% print()
+  }
+}
+
