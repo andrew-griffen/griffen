6872ac6c214cd9f2e37ebd22bdd9fe2005af9011
Andrew Griffen <andrew.griffen@gmail.com>
2024-10-26 11:51:46 +0900

    Updated pct function

--- R/pct.R
+++ R/pct.R
@@ -12,14 +12,26 @@ NULL
 #' pct(mtcars, cyl)
 
 pct <- function(df,...,.drop = FALSE){
-   if (!missing(...)) {
-     out <- group_by(df, ..., .add = TRUE, .drop = .drop)
-   } else {
-     out <- df
-   }
-  out <- out %>% dplyr::summarise(n = n(), .groups="drop") %>%
-  dplyr::mutate(pct = n / sum(n)) %>%
-  dplyr::select(-n)
-  out <- out %>% arrange(...,pct)
+  if (is_grouped_df(df)){
+      grp_df <- df |> summarise(n_grp = n())
+      out <- 
+      df |> 
+      group_by(..., .add = TRUE) |>
+      dplyr::summarise(n = n(), .groups="drop") |>
+      right_join(grp_df) |> 
+      dplyr::mutate(pct = n / n_grp) |>
+      dplyr::select(-n, -n_grp)            
+  }else{
+      if (!missing(...)) {
+          out <- group_by(df, ..., .add = TRUE, .drop = .drop)
+      } else {
+          out <- df
+      }
+      out <- 
+      out |> 
+      dplyr::summarise(n = n(), .groups="drop") |>
+      dplyr::mutate(pct = n / sum(n)) |>
+      dplyr::select(-n)                    
+  }
   return(out)
 }
