64da605ce7c74c359d2a07a492c539f10d77ccc3
andrew-griffen <andrew.griffen@gmail.com>
2025-02-10 14:13:25 +0900

    Added progress to read_csv

--- R/read_csv.R
+++ R/read_csv.R
@@ -1,11 +1,11 @@
 #' mean function with na.rm = TRUE
 #'
-#' Shuts up read_csv's incessant printing of crap
+#' Tries to shuts up read_csv's incessant printing of crap
 #' @param file a csv file
 #' @keywords read_csv na.rm
 #' @importFrom readr read_csv
 #' @export
 
-read_csv <- function(file, ..., show_col_types = FALSE) {
-  readr::read_csv(file, ..., show_col_types = show_col_types)
+read_csv <- function(file, ..., show_col_types = FALSE, progress = FALSE) {
+  readr::read_csv(file, ..., show_col_types = show_col_types, progress = progress)
 }
