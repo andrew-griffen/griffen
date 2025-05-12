5134e6b59145f38b14f4dd653cd546ca4b5fdc68
Andrew Griffen <andrew.griffen@gmail.com>
2025-05-12 15:19:18 +0900

    modified pct function

--- R/pct.R
+++ R/pct.R
@@ -11,27 +11,13 @@ NULL
 #' @examples
 #' pct(mtcars, cyl)
 
-pct <- function(df,...,.drop = FALSE){
-  if (is_grouped_df(df)){
-      grp_df <- df |> summarise(n_grp = n())
-      out <- 
-      df |> 
-      group_by(..., .add = TRUE) |>
-      dplyr::summarise(n = n(), .groups="drop") |>
-      right_join(grp_df) |> 
-      dplyr::mutate(pct = n / n_grp) |>
-      dplyr::select(-n, -n_grp)            
-  }else{
+pct <- function (df, ..., .drop = FALSE){
       if (!missing(...)) {
-          out <- group_by(df, ..., .add = TRUE, .drop = .drop)
-      } else {
-          out <- df
+          df <- group_by(df, ..., .add = TRUE, .drop = .drop)
       }
-      out <- 
-      out |> 
-      dplyr::summarise(n = n(), .groups="drop") |>
-      dplyr::mutate(pct = n / sum(n)) |>
-      dplyr::select(-n)                    
-  }
-  return(out)
+      out <- dplyr::select(dplyr::mutate(dplyr::summarise(df, 
+          n = n(), .groups = "drop"), pct = n/sum(n)), -n)
+      return(out)
 }
+
+
